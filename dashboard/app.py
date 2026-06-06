85976858e80749718f8cb0918987e8f2cdc213c0
Cam <camxcreative@gmail.com>
2026-06-06 19:50:39 -0400

    begin dashboard

---
+++ dashboard/app.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+from faicons import icon_svg
+
+# Import data from shared.py
+from shared import app_dir, df
+
+from shiny import reactive
+from shiny.express import input, render, ui
+
+ui.page_opts(title='NBA Dashboard', fillable=True)
